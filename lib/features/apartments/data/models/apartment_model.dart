823cb7e19d9bedd3562f71ee49faa8e7833acf1c
adilet <ibraevadilet3@gmail.com>
2022-11-16 14:11:33 +0600

    first commit

---
+++ lib/features/apartments/data/models/apartment_model.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class ApartmentModel {
+  final String numberOfRooms;
+  final String image;
+  final String price;
+  final String companyName;
+  final String firstPaymant;
+  ApartmentModel({
+    required this.numberOfRooms,
+    required this.image,
+    required this.price,
+    required this.companyName,
+    required this.firstPaymant,
+  });
+}
